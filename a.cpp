8c546a6c64efdddd1fa46cb249468ae64599aa88
NEURON <50287102+neuron909@users.noreply.github.com>
2020-10-19 19:47:43 +0530

    Added: CPP - template

---
+++ a.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FIN  freopen( "i.txt" , "r", stdin );
+#define FOUT freopen ("o.txt", "w", stdout );
+
+#define fo(  var, init, till       ) for( int var = (init); var < (till); ++var         )
+#define fos( var, init, till, step ) for( int var = (init); var < (till); var += (step) )
+
+#define rfo(  var, init, till       ) for( int var = (init); var >= (till); --var         )
+#define rfos( var, init, till, step ) for( int var = (init); var >= (till); var -= (step) )
+
+#define foe( val, vals ) for( auto &val : vals )
+
+typedef long long ll; typedef unsigned long long ull;
+
+typedef vector<int> vi; typedef vector<string> vs;
+typedef vector< vi > vvi; typedef vector< vs > vvs;
+
+#define in_i( var ) int var; cin >> var;
+#define in_s( var ) string var; cin >> var;
+
+#define in_vi( var, size ) vi var( size ); foe( each, var ) cin >> each;
+#define in_vs( var, size ) vs var( size ); foe( each, var ) cin >> each;
+
+#define in_vvi( var_vvi, size_r, size_c ) vvi var_vvi( size_r, vi(size_c) ); foe( var_vi, var_vvi ) foe( each, var_vi ) cin >> each;
+#define in_vvs( var_vvs, size_r, size_c ) vvs var_vvs( size_r, vs(size_c) ); foe( var_vs, var_vvs ) foe( each, var_vs ) cin >> each;
+
+#define deb_v( var_v )   foe( each , var_v  ) cout << each << " "; cout << '\n';
+#define deb_vv( var_vv ) foe( var_v, var_vv ) deb_v( var_v )     ; cout << '\n';
+
+
+int main() {
+FAST_IO;
+FIN;
+// FOUT;
+
+//----- Code ----- //
+    
+
+
+
+//----- Code ----- //
+    return 0;
+}

4be7d7c3fa43b97413d6ae57d1a9aeea172e4880
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-02-25 17:01:18 +0530

    style(sort): bubble sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -2,8 +2,8 @@ const arr = [1, 2, 3, 4, 5, 6]
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; ++i) {
-    for (let j = i+1; j < arr.length; ++j) {
-      if ( arr[i] <= arr[j] ) continue
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[i] <= arr[j]) continue
       const t = arr[i]
       arr[i] = arr[j]
       arr[j] = t
